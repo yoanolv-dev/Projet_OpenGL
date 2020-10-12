ea39b4cf827f01217d8567b0bef9b8e0fe0d62de
kxz-security <kxz-security@users.noreply.github.com>
2020-10-12 16:36:49 +0000

    TEST

--- Program.cs
+++ Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
 namespace Projet_OpenGL
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			Glut.glutInit();
+			Initialisation_3D();
+			Glut.glutReshapeFunc(On_Changement_Taille_Fenetre);
+			Glut.glutDisplayFunc(Afficher_Ma_Scene);
+			Glut.glutKeyboardFunc(Gestion_Clavier);
+			Glut.glutSpecialFunc(Gestion_Touches_Speciales);
+			Glut.glutIdleFunc(Animation_Scene);
+			Glut.glutMouseFunc(Gestion_Bouton_Souris);
+			Glut.glutMouseWheelFunc(Gestion_Molette);
+			Glut.glutPassiveMotionFunc(Gestion_Souris_Libre);
+			Glut.glutMotionFunc(Gestion_Souris_Clique);
+
+			Initialisation_Animation();
+			Glut.glutMainLoop();
+		}
+	}
 }
